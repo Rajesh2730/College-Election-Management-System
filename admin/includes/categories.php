804311cc6111cf6c0f79d8aadedbf350482fbebf
Rajesh <115982316+rajesh2724@users.noreply.github.com>
2024-05-13 18:10:18 +0530

    Add files via upload

---
+++ admin/includes/categories.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    <nav class="nav_bar">
+        
+        <button class="nav_btn" type="button" id="signup">Signup</button>
+        <button class="nav_btn" type="button" id="lout">LogOut</button>
+        <button class="nav_btn" type="button" id="muser">Manage User's</button>
+        <button class="nav_btn" type="button" id="nomini">Nominination's</button>
+        <button class="nav_btn" type="button" id="eresult">Election Result</button>
+    </nav>
+    </div>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,1 +1,32 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+<div class="header">
+<div class="cont1">
+    <a href="home.php" ><div class="ref_head"><img src="../logo/logo.jpg" class="logo1"><a href="home.php" class="logo_txt">CEMS-Admin</a></div></a>
+</div>
+<div class="cont2">
+        <input type="button" value="Home" class="header-btn" id="home">&nbsp;&nbsp;&nbsp;&nbsp;
+        
+        </div>
+        <div class="cont3"> 
+        <button class="header_btn1" id="profile"><img src="../user_profile/Navaneethan.jpg" class="profile_img"><?php
 
+// Check if the image path is stored in session
+echo '<div class="pro_img">';
+if (isset($_SESSION['img'])) {
+    // Display image from session variable
+} else {
+    echo '';
+}
+echo '</div>';
+echo '<div class="pro_name">';
+   if(isset($_SESSION['un'])){
+       echo "".$_SESSION['un'];
+       
+   }
+   else{
+       echo "GuestUser";
+   }  
+echo '</div>'; 
+   ?></button>
+   </div>
+   </div>
